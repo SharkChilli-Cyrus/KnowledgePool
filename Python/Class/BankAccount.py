984536db9c8e7faf46257a14ebf233d775c0ca54
xuzhu <cyrus.zhuxu@gmail.com>
2020-07-26 21:28:33 +0800

    daily update

--- Python/Class/BankAccount.py
+++ Python/Class/BankAccount.py
@@ -2,7 +2,7 @@ class BankAccount(object):
 
     # Constructor
     def __init__(self, accountNumber, accountName, balance):
-        self.accountName = accountNumber
+        self.accountNumber = accountNumber
         self.accountName = accountName
         self.balance = balance
     
@@ -12,6 +12,15 @@ class BankAccount(object):
     def withdraw(self, amount):
         self.balance = self.balance + amount
     
+    def info(self):
+        text = """
+        ===== Bank Account Info =====
+        * Account Number: {0}
+        * Account Name: {1}
+        * Account Balance: {2}
+        =============================
+        """.format(self.accountNumber, self.accountName, self.balance)
+        return text
 
 test_account1 = BankAccount(12345, "Tom", 100.0)
-print(test_account1.accountName)
+print(test_account1.info())
