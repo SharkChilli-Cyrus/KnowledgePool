b76ba8def2e16fe5fe08f439b0fb733f274b9a4a
Eric <cyrus.zhuxu@gmail.com>
2020-07-26 22:33:37 +0800

    daily update

--- Python/Class/BankAccount.py
+++ Python/Class/BankAccount.py
@@ -1,5 +1,5 @@
-class BankAccount(object):
-
+class BankAccount:
+    
     # Constructor
     def __init__(self, accountNumber, accountName, balance):
         self.accountNumber = accountNumber
@@ -7,20 +7,38 @@ class BankAccount(object):
         self.balance = balance
     
     def deposit(self, amount):
-        self.balance = self.balance - amount
-    
-    def withdraw(self, amount):
         self.balance = self.balance + amount
-    
+
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+
     def info(self):
         text = """
-        ===== Bank Account Info =====
+        ====== BankAccount Info =====
         * Account Number: {0}
         * Account Name: {1}
         * Account Balance: {2}
         =============================
         """.format(self.accountNumber, self.accountName, self.balance)
         return text
+    
+    def __str__(self):
+        return """
+        ====== BankAccount Info =====
+        * Account Number: {0}
+        * Account Name: {1}
+        * Account Balance: {2}
+        =============================
+        """.format(self.accountNumber, self.accountName, self.balance)
+    
+    def __lt__(self, other):
+        return self.balance < other.balance
+    
+    def __gt__(self, other):
+        return self.balance > other.balance
 
-test_account1 = BankAccount(12345, "Tom", 100.0)
-print(test_account1.info())
+
+test = BankAccount(1, 'Tom', 10000.0)
+
+print(test)
+print(test.info())
