8a4bdb0ef823584fa0bdf355aae16a76c43f976b
Eric <cyrus.zhuxu@gmail.com>
2020-08-05 21:25:19 +0800

    update Web

---
+++ Sample/Web_Auto.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8-sig -*-
+#
+# Web Auto
+#
+
+
+# @update time: Aug 2020
+# ====================================================================================================
+
+
+
+
+
+
+
+import os
+import time
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
