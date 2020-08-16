ffb33a94869a76387f3f4408a3e17e07f3c34b88
xuzhu <cyrus.zhuxu@gmail.com>
2020-08-16 19:52:06 +0800

    update API Doc

--- CodePool/GoogleSheet_API/Gsheet_API.py
+++ CodePool/GoogleSheet_API/Gsheet_API.py
@@ -40,20 +40,93 @@ def gsheet_GetCredentials(SCOPES, folder_path):
 
     return creds
 
+
+def gsheet_ReadTab(sheet, sheet_id, read_range):
+    request = sheet.values().get(spreadsheetId=sheet_id,
+                                 range=read_range)
+
+    response = request.execute()
+    values = response['values']
+    cols = values.pop(0)
+    df = pd.DataFrame(data = values, columns = cols)
+
+    return df
+
+
+def read_Gsheet_int(sheet, spreadsheet_id, read_range):
+    request = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                 range=read_range)
+    response = request.execute()
+    values = response['values']
+
+    query_list = []
+    for i in values:
+        if i == []:
+            i = [404404]
+        else:
+            i[0] = i[0].encode('utf-8')
+        query_list.append(i[0])
+
+    return query_list
+
+def read_Gsheet_str(sheet, spreadsheet_id, read_range):
+    request = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                 range=read_range)
+    response = request.execute()
+    values = response['values']
+
+    query_list = []
+    for i in values:
+        if i == []:
+            i = ['None']
+        else:
+            i[0] = i[0].encode('utf-8')
+        query_list.append(i[0])
+
+    return query_list
+
+# Upload data to GSheet
+def upload_csv(sheet, sheet_id, upload_range, df):
+    print('')
+    print('='*15, 'Upload csv To Google Sheet Begin', '='*15)
+
+    # Clear the sheet
+    clear_body = {}
+    clear_result = sheet.values().clear(spreadsheetId=sheet_id,
+                                        range=upload_range,
+                                        body=clear_body).execute()
+
+    df = df.replace(np.nan,'NA')
+
+    print('Google Sheet ID:', sheet_id)
+    print('Clear Range:', upload_range)
+    print('='*5, 'Clear Google Sheet Finished')
+
+    print('Upload DataFrame Shape:', df.shape)
+    print('GSheet Range:', upload_range)
+
+    body = {'values': df.values.tolist()}
+    result = sheet.values().update(spreadsheetId = sheet_id,
+                                   range = upload_range,
+                                   valueInputOption = 'RAW',
+                                   body = body).execute()
+
+    print('='*15, 'Upload csv To Google Sheet Finished', '='*15)
+    print('')
+    
 # ====================================================================================================
 
 
 
 
 # ====================================================================================================
+credential_folder = '/Users/xuzhu/Desktop/Keys/Gsheet_API' # AbsPath
 home_path = os.path.dirname(os.path.abspath(__file__))
-credential_folder = os.path.join(home_path, 'credentials')
 input_folder = os.path.join(home_path, 'InputFolder')
 print('')
 print("-------------------- Basic Info --------------------")
 print("* Credentials Folder Path: {0}".format(credential_folder))
 print("* Input Folder Path: {0}".format(input_folder))
-
 
 all_files = []
 add_text = []
@@ -61,9 +134,33 @@ add_text = []
 
 # Test
 gsheet_SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-# gsheet_SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 gsheet_creds = gsheet_GetCredentials(gsheet_SCOPES, credential_folder)
 service = build('sheets', 'v4', credentials=gsheet_creds)
 sheet = service.spreadsheets()
-# I:
-# 
+
+# I: Read Data From Google Sheet
+gsheet_id = "1nwnS2SeqFnym8vuuNz1Zt-HPXthvhZjHbbJiNYl9xiE"
+google_sheet_url = "https://docs.google.com/spreadsheets/d/{0}".format(gsheet_id)
+gsheet_read_range = "'Test 1'!A1:D"
+
+""" df = gsheet_ReadTab(sheet, gsheet_id, gsheet_read_range)
+
+request = sheet.values().get(spreadsheetId = gsheet_id,
+                             range = gsheet_read_range)
+
+response = request.execute()
+# response = {
+#     'range': "'Test 1'!A1:D1000", 
+#     'majorDimension': 'ROWS', 
+#     'values': [
+#                 ['column1', 'column2', 'column3', 'column4'],
+#                 ['Tom', '12.3', '56', '2020-08-01'],
+#                 ['Jerry', '2.6', '92', '2020-08-02']
+#               ]
+# }
+
+values = response['values']
+cols = values.pop(0) 
+df = pd.DataFrame(data = values, columns = cols)
+"""
+
